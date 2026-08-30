23260eeb07fe80a208e576477ddc29f27fb66134
Omar Tawil <omar.y.altawil@gmail.com>
2026-08-30 17:21:09 +0300

    Export six-variable second-order certificate

--- src/no_free_collapse/__init__.py
+++ src/no_free_collapse/__init__.py
@@ -22,12 +22,15 @@ from .gram import (
 )
 from .hafnian_bounds import (
     bounded_quadratic_hafnian_bound,
+    disjoint_pair_basis_blocks,
     disjoint_pair_hafnian_directional_derivative,
     disjoint_pair_hafnian_value,
     disjoint_pair_quadratic,
     disjoint_pair_tangent_invariants,
     full_parity_power_coefficient,
     hafnian,
+    six_variable_critical_hafnian_quadratic_coefficient,
+    six_variable_critical_path_second_order_bound,
     twice_offdiag,
 )
 from .interactions import interaction_degree, walsh_coefficient, walsh_coefficients
@@ -40,10 +43,12 @@ __all__ = [
     "spectral_tail_energy_bound", "truncation_bound", "parity_state_coefficients",
     "effect_gram", "feature_vector", "gram_probability", "monomial_masks",
     "optimal_absolute_linear_readout_for_fixed_Q", "optimal_linear_readout_for_fixed_Q",
-    "realize_gram_pair", "bounded_quadratic_hafnian_bound",
+    "realize_gram_pair", "bounded_quadratic_hafnian_bound", "disjoint_pair_basis_blocks",
     "disjoint_pair_hafnian_directional_derivative", "disjoint_pair_hafnian_value",
     "disjoint_pair_quadratic", "disjoint_pair_tangent_invariants",
-    "full_parity_power_coefficient", "hafnian", "twice_offdiag",
+    "full_parity_power_coefficient", "hafnian",
+    "six_variable_critical_hafnian_quadratic_coefficient",
+    "six_variable_critical_path_second_order_bound", "twice_offdiag",
     "interaction_degree", "walsh_coefficient", "walsh_coefficients",
     "matched_pair_asymptotic_rate", "matched_pair_capacity", "matched_pair_gram",
 ]
